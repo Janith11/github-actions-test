1641b567531d4596576818aa25f98089bd56ecba
Janith11 <janithpramuditha042@gmail.com>
2024-03-23 22:34:29 +0530

    git init for the git hub actions

---
+++ src/main/java/com/testcode/demogithubactiongke/DemoGithubActionGkeApplication.java
@@ -0,0 +1,13 @@
+package com.testcode.demogithubactiongke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionGkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubActionGkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcode/demogithubactiongke/DemoGithubActionGkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.demogithubactiongke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionGkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
